4bcd847ab521e4caa20783efcad537c48e19b517
charan <charanm927@gmail.com>
2024-08-23 13:12:18 +0530

    chore: create individual blog page

--- src/payload/blocks/Details/components/BlogDetails.tsx
+++ src/payload/blocks/Details/components/BlogDetails.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import { Blog } from '@payload-types'
-import Link from 'next/link'
-import { LiaBlogSolid } from 'react-icons/lia'
 
-import TabComponent, { TabContent } from '@/payload/blocks/common/Tabs'
+import BlogView from './BlogView'
+import PostScrollBar from './PostScrollBar'
 
 interface BlogDetailsProps {
   blog: Blog
@@ -12,38 +11,10 @@ interface BlogDetailsProps {
 }
 
 const BlogDetails: React.FC<BlogDetailsProps> = ({ blog, blogsData }) => {
-  console.log('blogs', blogsData)
-  const tabs = [
-    {
-      title: 'Blog Data',
-      id: 'BlogData',
-      icon: <LiaBlogSolid size={24} />,
-      color: '#5d5dff',
-      content: TabContent,
-      data: blog,
-    },
-    {
-      title: 'Blogs Data',
-      id: 'BlogsData',
-      icon: <LiaBlogSolid size={24} />,
-      color: '#5d5dff',
-      content: TabContent,
-      data: blogsData,
-    },
-  ]
   return (
-    <div className='mx-auto max-h-screen min-h-screen max-w-7xl  gap-6 overflow-hidden px-2'>
-      <div className='mt-4 flex items-center justify-between'>
-        <p className='rounded-rounded-box border-2 border-base-content/10 bg-base-content/20 px-4 py-2'>
-          Get Started with src/app/(app)/(marketing)/blog/[blogSlug]
-        </p>
-        <Link
-          href='/blogs'
-          className='rounded-rounded-box border-2 border-base-content/10 bg-base-content/20 px-4 py-2'>
-          back
-        </Link>
-      </div>
-      <TabComponent tabs={tabs} />
+    <div>
+      <PostScrollBar blog={blog} />
+      <BlogView blog={blog} />
     </div>
   )
 }
---
+++ src/payload/blocks/Details/components/PostScrollBar.tsx
@@ -0,0 +1,94 @@
+'use client'
+
+import Button from '../../common/Button'
+import Container from '../../common/Container'
+import { env } from '@env'
+import { Blog, Media } from '@payload-types'
+import { motion, useScroll, useSpring } from 'framer-motion'
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+import Copy from '@/svg/Copy'
+import TickMark from '@/svg/TickMark'
+
+const PostScrollBar = ({ blog }: { blog: Blog }) => {
+  const pathName = usePathname()
+  const [copied, setCopied] = useState(false)
+  const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  })
+
+  const [showContainer, setShowContainer] = useState(false)
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY
+    if (scrollPosition > 70) {
+      setShowContainer(true)
+    } else {
+      setShowContainer(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  function copyText(pathName: string) {
+    navigator.clipboard
+      .writeText(`${env.NEXT_PUBLIC_PUBLIC_URL}${pathName}`)
+      .then(() => {
+        setCopied(true)
+        setTimeout(() => setCopied(false), 2000)
+      })
+      .catch(err => console.error('Failed to copy text: ', err))
+  }
+  return (
+    <div className='sticky top-0 z-10 bg-base-100'>
+      {showContainer && (
+        <motion.div
+          initial={{ y: '-100%', opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ type: 'spring', stiffness: 100, damping: 20 }}>
+          <Container>
+            <div className='flex h-16 w-full items-center justify-between sm:h-[74px]'>
+              <div className='xs:gap-4 flex items-center justify-center gap-3'>
+                <div className='avatar'>
+                  <div className='w-10'>
+                    <Image
+                      alt='Author'
+                      fill
+                      className='rounded-full'
+                      src={(blog?.blogImage as Media)?.url!}
+                    />
+                  </div>
+                </div>
+                <div className='line-clamp-1 text-lg sm:font-semibold'>
+                  {blog?.title}
+                </div>
+              </div>
+              <Button
+                onClick={() => copyText(pathName)}
+                className={`hover:bg-base-150 } !rounded-full`}>
+                {copied ? <TickMark /> : <Copy />}
+                {copied ? 'Copied' : 'Copy Link'}
+              </Button>
+            </div>
+          </Container>
+        </motion.div>
+      )}
+      <motion.div
+        className='progress-bar h-1 origin-left bg-primary '
+        style={{ scaleX }}
+      />
+    </div>
+  )
+}
+
+export default PostScrollBar
