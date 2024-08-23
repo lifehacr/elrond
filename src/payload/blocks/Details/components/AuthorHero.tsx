b81e5181b65bc4350304506d354923f213d26d5a
charan <charanm927@gmail.com>
2024-08-23 15:12:41 +0530

    chore: create individual author page dynamic

---
+++ src/payload/blocks/Details/components/AuthorHero.tsx
@@ -0,0 +1,35 @@
+import Button from '../../common/Button'
+import { User } from '@payload-types'
+import Image from 'next/image'
+
+import Globe from '@/svg/Globe'
+import Twitter from '@/svg/Twitter'
+
+const AuthorHero = ({ author }: { author: User }) => {
+  return (
+    <div className='flex flex-col items-center bg-secondary pb-16 pt-14'>
+      <div className='avatar'>
+        <div className='relative w-24 rounded-full'>
+          <Image alt='Author' fill src={author?.imageUrl!} />
+        </div>
+      </div>
+      <div className='mt-6 text-xl font-bold'>{author?.displayName}</div>
+      <div className='mt-5 font-semibold text-neutral-content'>
+        {author?.city}
+      </div>
+      <p className='mt-5  max-w-sm text-center text-neutral-content'>
+        {author?.description}
+      </p>
+      <div className='mt-5 flex gap-4'>
+        <Button className='hover:bg-base-150 !rounded-full'>
+          <Twitter /> @hedwick
+        </Button>
+        <Button className='hover:bg-base-150 !rounded-full'>
+          <Globe /> cagatiya.io
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default AuthorHero
