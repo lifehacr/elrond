b727288df884ad55d4bc8b745dfd65239016083a
charan <charanm927@gmail.com>
2024-08-26 22:05:09 +0530

    chore: footer dynamic

--- src/payload/blocks/common/Footer.tsx
+++ src/payload/blocks/common/Footer.tsx
@@ -1,70 +1,95 @@
 import Container from '../common/Container'
+import { Media, Page, SiteSetting } from '@payload-types'
+import Image from 'next/image'
 import Link from 'next/link'
 
 import Facebook from '@/svg/Facebook'
 import Github from '@/svg/Github'
-import Logo from '@/svg/Logo'
 import Twitter from '@/svg/Twitter'
 
-const Footer = () => {
-  const x =
-    'w-full md:max-w-screen-container flex flex-col md:flex-row justify-between'
+const icons = {
+  facebook: <Facebook />,
+  github: <Github />,
+  twitter: <Twitter />,
+}
+
+const Footer = ({ initData }: { initData: SiteSetting }) => {
   return (
     <div className='border-t'>
       <Container className='md:max-w-screen-[940px] w-full px-4 pt-2'>
         <div className='flex flex-col justify-between gap-6 py-8 sm:py-12 md:flex-row md:gap-5'>
           <div className='flex flex-col justify-start gap-5'>
-            <Link href={'/'}>
-              <Logo className='h-6 w-fit' />
+            <Link href={'/'} className='relative h-6 w-28'>
+              <Image
+                alt={(initData?.logoImage as Media)?.alt || 'Logo'}
+                src={(initData?.logoImage as Media)?.url!}
+                fill
+              />
             </Link>
             <p className='text-lg font-normal leading-normal md:max-w-xs'>
-              A super minimal & lightweight theme with Premium Membership and
-              fully Ghost-compatible.
+              {initData?.appDescription}
             </p>
           </div>
           <div className='flex gap-16'>
-            <div className='flex flex-col items-start justify-start gap-3'>
-              <div className='mb-2 text-xs font-[450] uppercase tracking-widest text-[#3F3F46]'>
-                membership
+            {initData?.footer?.links?.map((footerLink, index) => (
+              <div
+                key={index}
+                className='flex flex-col items-start justify-start gap-3'>
+                <div className='mb-2 text-xs font-[450] uppercase tracking-widest text-[#3F3F46]'>
+                  {footerLink?.group && footerLink?.menuLinkGroup?.groupTitle}
+                </div>
+                <div>
+                  <ul className='flex flex-col gap-3 text-[15px] font-light leading-[18px] text-neutral-content underline hover:text-base-content sm:no-underline'>
+                    {footerLink?.group ? (
+                      footerLink?.menuLinkGroup?.groupLinks?.map(
+                        (link, index) =>
+                          link?.externalLink ? (
+                            <Link
+                              target={`${link?.newPage ? '_blank' : '_self'}`}
+                              key={index}
+                              href={link?.link!}>
+                              {link?.label}
+                            </Link>
+                          ) : (
+                            <Link
+                              key={index}
+                              target={`${link?.newPage ? '_blank' : '_self'}`}
+                              href={(link?.page?.value as Page)?.slug!}>
+                              {(link?.page?.value as Page)?.title}
+                            </Link>
+                          ),
+                      )
+                    ) : footerLink?.menuLink?.externalLink ? (
+                      <Link
+                        target={`${footerLink?.menuLink?.newPage ? '_blank' : '_self'}`}
+                        href={footerLink?.menuLink?.link!}>
+                        {footerLink?.menuLink?.label}
+                      </Link>
+                    ) : (
+                      <Link
+                        target={`${footerLink?.menuLink?.newPage ? '_blank' : '_self'}`}
+                        href={
+                          (footerLink?.menuLink?.page?.value as Page)?.slug!
+                        }>
+                        {(footerLink?.menuLink?.page?.value as Page)?.title}
+                      </Link>
+                    )}
+                  </ul>
+                </div>
               </div>
-              <div>
-                <ul className='flex flex-col gap-3 text-[15px] font-light leading-[18px] text-neutral-content underline hover:text-base-content sm:no-underline'>
-                  <Link href={'/membership'}>Plans</Link>
-                  <Link href={'/sign-in'}>Sign In</Link>
-                  <Link href={'/sign-up'}>Sign Up</Link>
-                  <Link href={'/subscribe'}>Subscribe</Link>
-                  <Link href={'/'}>Portal</Link>
-                </ul>
-              </div>
-            </div>
-            <div className='flex flex-col items-start justify-start gap-3'>
-              <div className='mb-2 text-xs font-[450] uppercase tracking-widest text-[#3F3F46]'>
-                links
-              </div>
-              <div>
-                <ul className='flex flex-col gap-3 text-[15px] font-light leading-[18px] text-neutral-content underline hover:text-base-content sm:no-underline'>
-                  <Link href={'/contact'}>Contact</Link>
-                  <Link href={'/404'}>404 Page</Link>
-                  <Link href={'/documentation'}>Documentation</Link>
-                </ul>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
         <div className='flex w-full flex-col-reverse items-center justify-center gap-4 py-4 text-center sm:flex-row  sm:items-center sm:justify-between sm:gap-12'>
           <p className='py-2 text-sm font-light text-[#3F3F46]'>
-            © 2024 Elrond — Published with Ghost & Elrond
+            {initData?.footer?.copyright}
           </p>
           <div className='flex gap-5'>
-            <Link href={'/'}>
-              <Facebook />
-            </Link>
-            <Link href={'/'}>
-              <Twitter />
-            </Link>
-            <Link href={'/'}>
-              <Github />
-            </Link>
+            {initData?.footer?.socialLinks?.map((link, index) => (
+              <Link key={index} href={link?.socialMediaLink || '/'}>
+                {icons[link?.socialMedia]}
+              </Link>
+            ))}
           </div>
         </div>
       </Container>
