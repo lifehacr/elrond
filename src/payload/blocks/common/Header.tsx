3f097992c51bf97a22a54867142333c22b96304d
charan <charanm927@gmail.com>
2024-08-27 18:51:43 +0530

    chore: resolve conflict

--- src/payload/blocks/common/Header.tsx
+++ src/payload/blocks/common/Header.tsx
@@ -3,9 +3,7 @@
 import Button from '../common/Button'
 import Container from '../common/Container'
 import DropDown from '../common/DropDown'
-
 import { Media, Page, SiteSetting } from '@payload-types'
-
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
@@ -38,7 +36,6 @@ const Header = ({ initData }: { initData: SiteSetting }) => {
     router.push('/sign-in')
   }
 
-
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(e.target.value)
     e.target.value.length >= 1
@@ -46,8 +43,13 @@ const Header = ({ initData }: { initData: SiteSetting }) => {
       : ''
   }
 
+  function capitalizeFirstLetter(string: string) {
+    if (!string) return string
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
   return (
-    <div className=''>
+    <div className='bg-base-100'>
       <Modal
         open={open}
         onClose={() => {
@@ -137,53 +139,38 @@ const Header = ({ initData }: { initData: SiteSetting }) => {
         </div>
       </Modal>
       <Container className='z-50 flex h-20 items-center justify-between bg-base-100 px-4 xl:px-0'>
-        <div className='flex-[1] justify-start'>
-          <Link href={'/'}>
-            <Logo className='h-5 w-fit' />
-          </Link>
-        </div>
-        <nav className='mx-auto hidden h-full w-fit select-none items-center justify-center gap-6 px-4 lg:flex'>
-          <ul className='mx-auto flex w-fit items-center gap-6 px-4 text-base font-[450] text-[#3F3F46]'>
-            <Link
-              href={'/style-guide'}
-              className={
-                pathName === '/style-guide' ? 'text-secondary-content' : ''
-              }>
-              Style Guide{' '}
-              <span className='inline-block bg-gradient-to-r from-[#FED7AA] to-[#F97316] bg-clip-text text-transparent'>
-                ✦
-              </span>
-            </Link>
-            <Link
-              href={'/features'}
-              className={
-                pathName === '/features' ? 'text-secondary-content' : ''
-              }>
-              Features
-            </Link>
-            <Link
-              href={'/membership'}
-              className={
-                pathName === '/membership' ? 'text-secondary-content' : ''
-              }>
-              Membership
-            </Link>
-            <Link
-              href={'/author'}
-              className={
-                pathName === '/author' ? 'text-secondary-content' : ''
-              }>
-              Authors
-            </Link>
-            <Link
-              href={'/tag'}
-              className={pathName === '/tag' ? 'text-secondary-content' : ''}>
-              Tags
-            </Link>
-            <DropDown />
+        <Link href={'/'} className='relative h-5 w-24'>
+          <Image alt='' src={(initData?.logoImage as Media)?.url!} fill />
+        </Link>
+        <nav className='mx-auto hidden h-full select-none items-center justify-center lg:flex'>
+          <ul className='flex items-center gap-6 text-base font-[450] text-[#3F3F46]'>
+            {initData?.header?.menuLinks?.map((headerLink, index) => (
+              <ul
+                key={index}
+                className='flex items-center gap-6 text-base font-[450] text-[#3F3F46]'>
+                {headerLink?.group ? (
+                  <DropDown headerLink={headerLink} />
+                ) : headerLink?.menuLink?.externalLink ? (
+                  <Link
+                    target={`${headerLink?.menuLink?.newPage ? '_blank' : '_self'}`}
+                    href={headerLink?.menuLink?.link!}>
+                    {capitalizeFirstLetter(headerLink?.menuLink?.label!)}
+                  </Link>
+                ) : (
+                  <Link
+                    target={`${headerLink?.menuLink?.newPage ? '_blank' : '_self'}`}
+                    href={(headerLink?.menuLink?.page?.value as Page)?.slug!}>
+                    {capitalizeFirstLetter(
+                      (headerLink?.menuLink?.page?.value as Page)?.title,
+                    )}
+                  </Link>
+                )}
+              </ul>
+            ))}
           </ul>
         </nav>
-        <div className='xs:gap-x-4 flex h-full w-fit min-w-fit flex-[1] items-center justify-end gap-x-3'>
+        <div className='xs:gap-x-4 flex h-full items-center justify-end gap-x-3'>
+          {' '}
           <Button
             onClick={() => setOpen(true)}
             className='h-[34px] w-[34px] !rounded-full bg-neutral-content bg-opacity-5 px-1 hover:bg-inherit'>
@@ -195,75 +182,22 @@ const Header = ({ initData }: { initData: SiteSetting }) => {
               onClick={handleSignPage}>
               <span className='hidden text-inherit sm:inline'>✦</span>
               <span className='sm:inline'> logout</span>
-
-  function capitalizeFirstLetter(string: string) {
-    if (!string) return string
-    return string.charAt(0).toUpperCase() + string.slice(1)
-  }
-
-  return (
-    pathName !== '/contact' &&
-    pathName !== '/subscribe' && (
-      <div className='bg-base-100'>
-        <Container className='flex h-20 items-center justify-between bg-base-100 px-4 xl:px-0'>
-          <Link href={'/'} className='relative h-5 w-24'>
-            <Image alt='' src={(initData?.logoImage as Media)?.url!} fill />
-          </Link>
-          <nav className='mx-auto hidden h-full select-none items-center justify-center lg:flex'>
-            <ul className='flex items-center gap-6 text-base font-[450] text-[#3F3F46]'>
-              {initData?.header?.menuLinks?.map((headerLink, index) => (
-                <ul
-                  key={index}
-                  className='flex items-center gap-6 text-base font-[450] text-[#3F3F46]'>
-                  {headerLink?.group ? (
-                    <DropDown headerLink={headerLink} />
-                  ) : headerLink?.menuLink?.externalLink ? (
-                    <Link
-                      target={`${headerLink?.menuLink?.newPage ? '_blank' : '_self'}`}
-                      href={headerLink?.menuLink?.link!}>
-                      {capitalizeFirstLetter(headerLink?.menuLink?.label!)}
-                    </Link>
-                  ) : (
-                    <Link
-                      target={`${headerLink?.menuLink?.newPage ? '_blank' : '_self'}`}
-                      href={(headerLink?.menuLink?.page?.value as Page)?.slug!}>
-                      {capitalizeFirstLetter(
-                        (headerLink?.menuLink?.page?.value as Page)?.title,
-                      )}
-                    </Link>
-                  )}
-                </ul>
-              ))}
-            </ul>
-          </nav>
-          <div className='xs:gap-x-4 flex h-full items-center justify-end gap-x-3'>
-            <Button className='h-[34px] w-[34px] !rounded-full bg-neutral-content bg-opacity-5 px-1 hover:bg-inherit'>
-              <SearchIcon />
-
             </Button>
-            {data?.username ? (
-              <Button
-                className='h-[34px] !rounded-full bg-primary font-medium text-white'
-                onClick={handleSignPage}>
-                <span className='hidden text-inherit sm:inline'>✦</span>
-                <span className='sm:inline'> logout</span>
-              </Button>
-            ) : (
-              <Button
-                className='h-[34px] !rounded-full bg-primary font-medium text-white'
-                onClick={handleSignPage}>
-                <span className='hidden text-inherit sm:inline'>✦</span>
-                <span className='hidden sm:inline'> Sign in</span>
-                <LockIcon className='inline sm:hidden' />
-              </Button>
-            )}
-            <Button className='h-[34px] w-[34px] !rounded-full p-0 lg:hidden'>
-              <MenuIcon />
+          ) : (
+            <Button
+              className='h-[34px] !rounded-full bg-primary font-medium text-white'
+              onClick={handleSignPage}>
+              <span className='hidden text-inherit sm:inline'>✦</span>
+              <span className='hidden sm:inline'> Sign in</span>
+              <LockIcon className='inline sm:hidden' />
             </Button>
-          </div>
-        </Container>
-      </div>
-    )
+          )}
+          <Button className='h-[34px] w-[34px] !rounded-full p-0 lg:hidden'>
+            <MenuIcon />
+          </Button>
+        </div>
+      </Container>
+    </div>
   )
 }
 
