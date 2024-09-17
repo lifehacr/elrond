68c40b65e5f5e2acb1fd302fbaa7a19ff034c185
charan <charanm927@gmail.com>
2024-09-17 19:20:26 +0530

    chore: create route to contact submission

--- src/trpc/routers/index.ts
+++ src/trpc/routers/index.ts
@@ -6,6 +6,7 @@ import { siteSettingsRouter } from '@/trpc/routers/site-settings'
 import { tagRouter } from '@/trpc/routers/tag'
 
 import { authRouter } from './auth'
+import { contactRouter } from './contact'
 import { searchRouter } from './search'
 import { seedRouter } from './seed'
 import { userRouter } from './user/user-route'
@@ -20,6 +21,7 @@ export const appRouter = router({
   user: userRouter,
   seed: seedRouter,
   search: searchRouter,
+  contact: contactRouter,
 })
 
 export type AppRouter = typeof appRouter
