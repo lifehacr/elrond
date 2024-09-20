37029cb06e009a608bc88d4b447639f847481442
charan <charanm927@gmail.com>
2024-09-20 15:42:49 +0530

    chore: re-initialization

--- src/payload/blocks/common/Commento.tsx
+++ src/payload/blocks/common/Commento.tsx
@@ -1,19 +1,39 @@
 import { useEffect } from 'react'
 
+// Extend the global window object to include Commento
+declare global {
+  interface Window {
+    Commento: {
+      start: () => void
+    }
+  }
+}
+
 const Commento = () => {
   useEffect(() => {
-    // Check if Commento script is already loaded to avoid re-loading it
     if (!document.getElementById('commento-script')) {
       const script = document.createElement('script')
       script.src = 'https://cdn.commento.io/js/commento.js'
       script.defer = true
+      script.id = 'commento-script'
       script.setAttribute('data-css-override', '/commento-custom.css')
-      script.id = 'commento-script' // Give the script an ID to avoid duplication
       document.body.appendChild(script)
-    }
-  }, []) // Only run this once when the component mounts
 
-  return <div id='commento'></div> // This is where the Commento widget will render
+      // Ensure Commento is initialized once the script is loaded
+      script.onload = () => {
+        if (window.Commento) {
+          window.Commento.start()
+        }
+      }
+    } else {
+      // Reinitialize Commento if the script is already loaded
+      if (window.Commento) {
+        window.Commento.start()
+      }
+    }
+  }, [])
+
+  return <div id='commento'></div>
 }
 
 export default Commento
