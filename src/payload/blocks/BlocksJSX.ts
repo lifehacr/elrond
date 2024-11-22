92f34e84b77853c14436c1288ffb70e5f610f204
charan <charanm927@gmail.com>
2024-11-22 16:49:30 +0530

    chore: blocks dynamic imports

--- src/payload/blocks/BlocksJSX.ts
+++ src/payload/blocks/BlocksJSX.ts
@@ -1,17 +1,20 @@
 import { DisqusComments } from '@contentql/core/client'
+import dynamic from 'next/dynamic'
 
-import { Contact } from './Contact'
-import { Details } from './Details'
-import { FAQ } from './FAQ'
-import { FeaturedPost } from './FeaturedPost'
-import { Features } from './Features'
-import { FormBlock } from './Form'
-import { Hero } from './Hero'
-import { LatestPosts } from './LatestPosts'
-import { List } from './List'
-import { Pricing } from './Pricing'
-import { RecommendationsList } from './RecommendationsList'
-import { Subscribe } from './Subscribe'
+const Contact = dynamic(() => import('./Contact/component'))
+const Details = dynamic(() => import('./Details/component'))
+const FAQ = dynamic(() => import('./FAQ/component'))
+const FeaturedPost = dynamic(() => import('./FeaturedPost/component'))
+const LatestPosts = dynamic(() => import('./LatestPosts/component'))
+const Pricing = dynamic(() => import('./Pricing/component'))
+const RecommendationsList = dynamic(
+  () => import('./RecommendationsList/component'),
+)
+const Subscribe = dynamic(() => import('./Subscribe/component'))
+const List = dynamic(() => import('./List/component'))
+const FormBlock = dynamic(() => import('./Form/component'))
+const Hero = dynamic(() => import('./Hero/component'))
+const Features = dynamic(() => import('./Features/component'))
 
 // Exporting an object that maps block names (as keys) to their corresponding JSX components (as values)
 // This object allows dynamic rendering of components based on the block names
